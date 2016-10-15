28aba3f37c77546319ac28499dab4b86884ae77e
dross <davidrosstw@gmail.com>
2016-10-15 13:35:54 -0500

    Got some error checking in place

--- components/math/base-ten/base-ten.js
+++ components/math/base-ten/base-ten.js
@@ -9,6 +9,7 @@
 
     function BaseTenCtrl($state, $location) {
         var self = this;
+        self.badNumError = false;
         self.qNumber = null;
         self.qCopy = null;
         self.hundreds = null;
@@ -22,27 +23,34 @@
         self.wrong2 = null;
 
         self.setQNumber = function(goalNum, startNum, endNum) {
-            if (goalNum == null) {
-                self.qNumber = Math.floor(Math.random() * ((endNum - startNum) + 1) + startNum);
-            }
-            self.qCopy = angular.copy(self.qNumber);
-            self.hundreds = null;
-            self.hundredsArray = [];
-            self.tens = null;
-            self.tensArray = [];
-            self.ones = null;
-            self.onesArray = [];
-            self.hundreds = Math.floor((self.qNumber % 1000) / 100);
-            for (var a = 0; a < self.hundreds; a++) {
-                self.hundredsArray.push(a);
-            }
-            self.tens = Math.floor((self.qNumber % 100) / 10);
-            for (var b = 0; b < self.tens; b++) {
-                self.tensArray.push(b);
-            }
-            self.ones = self.qNumber % 10;
-            for (var c = 0; c < self.ones; c++) {
-                self.onesArray.push(c);
+            if (angular.isUndefined(goalNum) || goalNum == null && (startNum == null || endNum == null)) {
+                self.badNumError = true;
+                self.qNumber = null;
+                self.qCopy = null;
+            } else {
+                self.badNumError = false;
+                if (goalNum == null) {
+                    self.qNumber = Math.floor(Math.random() * ((endNum - startNum) + 1) + startNum);
+                }
+                self.qCopy = angular.copy(self.qNumber);
+                self.hundreds = null;
+                self.hundredsArray = [];
+                self.tens = null;
+                self.tensArray = [];
+                self.ones = null;
+                self.onesArray = [];
+                self.hundreds = Math.floor((self.qNumber % 1000) / 100);
+                for (var a = 0; a < self.hundreds; a++) {
+                    self.hundredsArray.push(a);
+                }
+                self.tens = Math.floor((self.qNumber % 100) / 10);
+                for (var b = 0; b < self.tens; b++) {
+                    self.tensArray.push(b);
+                }
+                self.ones = self.qNumber % 10;
+                for (var c = 0; c < self.ones; c++) {
+                    self.onesArray.push(c);
+                }
             }
         };
     }
